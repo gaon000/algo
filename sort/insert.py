80f6af4753d7b0f0258164934b256e925990aabe
gaon000 <gaon000@naver.com>
2021-11-13 01:33:12 +0900

    use shift

--- sort/insert.py
+++ sort/insert.py
@@ -3,9 +3,19 @@
 arr = list(map(int, input().split(',')))
 
 for i in range(1, len(arr)):
+    '''
     for j in range(i, 0, -1):
         if arr[j] < arr[j-1]:
             arr[j], arr[j-1] = arr[j-1], arr[j]
         else:
             break
+    '''
+    key = arr[i]
+    j = i
+    while j > 0 and key < arr[j-1]:
+        arr[j] = arr[j-1]
+        j -= 1
+    arr[j] = key 
+
+
 print(arr)
